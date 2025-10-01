2e7e6f564f43cb8dde1f728211a44dac718b98d7
Sizning Ismingiz <email@example.com>
2025-10-01 14:07:28 +0500

    Initial commit with pom.xml and project files

--- src/main/java/org/example/AdminBot.java
+++ src/main/java/org/example/AdminBot.java
@@ -8,9 +8,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AdminBot extends TelegramLongPollingBot {
-    private static final String BOT_TOKEN = Env.get("ADMIN_BOT_TOKEN");
-    private static final String BOT_USERNAME = Env.get("ADMIN_BOT_USERNAME");
-    private static final String ADMIN_CHAT_ID = Env.get("ADMIN_CHAT_ID");
+    private static final String BOT_TOKEN = "8295381933:AAFgcq71yiksMshiKw11JBc64qE1QAwtOE4";
+    private static final String BOT_USERNAME = "answer812_bot";
+    private static final String ADMIN_CHAT_ID = "6448561095";
 
     private static AdminBot instance; // singleton
 
--- src/main/java/org/example/ServiceBot.java
+++ src/main/java/org/example/ServiceBot.java
@@ -12,8 +12,8 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 import java.util.*;
 
 public class ServiceBot extends TelegramLongPollingBot {
-    private static final String BOT_TOKEN = Env.get("SERVICE_BOT_TOKEN");
-    private static final String BOT_USERNAME = Env.get("SERVICE_BOT_USERNAME");
+    private static final String BOT_TOKEN = "8449488730:AAHa5Q9xH7tXckbGLyO6twT1SB-QnCIHrcQ";
+    private static final String BOT_USERNAME = "Konspek1_bot";
     private static final String DEFAULT_PROMO = "SYNOPSIS_2026";
 
     private final Map<String, Boolean> promoUsed = new HashMap<>();
