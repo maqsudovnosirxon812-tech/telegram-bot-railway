530661447693a0645369e6ba8cc4804a4618c50f
Sizning Ismingiz <email@example.com>
2025-10-07 17:34:39 +0500

    Initial commit - Telegram bots project

---
+++ src/main/java/org/example/BotConfig.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class BotConfig {
+    // ✅ Token va username'lar to‘g‘ridan-to‘g‘ri yozilgan
+    public static final String SERVICE_TOKEN = "8449249079:AAEytR-WTUSL5jJpjNdz99Kl2r7OAvpJd2Q";
+    public static final String SERVICE_USERNAME = "Konspek1_bot";
+
+    public static final String ADMIN_TOKEN = "8295525954:AAEmIv-R-Bu7DCTW65f19o11_7VVN7RH_pY";
+    public static final String ADMIN_USERNAME = "answer812_bot";
+}
